483867e7108525f3cf7492596fb2fabaf69e6157
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-30 11:34:56 +0100

    added the main App.js html body

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body class="body body--backgroundImage">
+      <form class="form js-form">
+        <fieldset class="form__fieldset">
+          <legend class="form__legend">CURRENCY CALCULATOR</legend>
+          <p>
+            <label class="form__label">Amount:
+              <input class="form__input js-amount" type="number" name="amount" placeholder="Wpisz kwotę - waluta PLN"
+                step="0.01" max="10000000000" min="0.01" required autofocus />
+            </label>
+          </p>
+          <p>
+            <label class="form__label">Choose currency:
+              <select class="form__select js-currency" name="currency" required>
+                <option value="EUR">EUR - euro</option>
+                <option value="USD">USD - U.S. dollar</option>
+                <option value="GBP">GBP - pound sterling</option>
+              </select>
+            </label>
+          </p>
+        </fieldset>
+        <button class="form__button">Calculate</button>
+        <p><strong class="js-result form__result">Here you will see the result</strong></p>
+      </form>
+    </body>
   );
 }
 
