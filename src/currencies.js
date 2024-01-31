7d731c3775a5b77a8441ebd8e9701e078b168708
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-31 09:10:21 +0100

    added separate file for "currencies" table

---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+export const currencies = [
+    {
+        name: "EUR - Euro",
+        ratio: 4.36,
+        symbol: "EUR",
+    },
+    {
+        name: "USD - Dolar amerykański",
+        ratio: 4.03,
+        symbol: "USD",
+    },
+    {
+        name: "GBP - Funt brytyjski",
+        ratio: 5.10,
+        symbol: "GBP",
+    },
+];
