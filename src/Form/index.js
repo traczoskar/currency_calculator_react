af9b56dd5145a7621e87537fb2b0c0a6150baf4a
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-31 22:24:23 +0100

    changed Form component to work with flags render, changed button code and few minor things

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,54 +3,64 @@ import { currencies } from "../currencies";
 import { Result } from "../Result";
 import "./style.css";
 
-
 const Form = ({ title, calculateResult, result }) => {
-    const [selectedCurrency, setSelectedCurrency] = useState(currencies[0].symbol);
-    const [amount, setAmount] = useState("");
+  const [selectedCurrency, setSelectedCurrency] = useState(
+    currencies[0].symbol
+  );
+  const [amount, setAmount] = useState("");
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        calculateResult(selectedCurrency, amount);
-    };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateResult(selectedCurrency, amount);
+  };
 
-    return (
-        <form className="form" onSubmit={onSubmit}>
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">{title}</legend>
-                <label className="form__label">
-                    Amount:
-                    <input
-                        className="form__input"
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        type="number"
-                        placeholder="Wpisz kwotę - waluta PLN"
-                        step="0.01"
-                        max="10000000000"
-                        min="0.01"
-                        required />
-                </label>
-                <label className="form__label">Choose currency:
-                    <select
-                        className="form__select"
-                        value={selectedCurrency}
-                        onChange={({ target }) => setSelectedCurrency(target.value)}
-                        name="currency"
-                        required>
-                        {currencies.map((currency) => (
-                            <option
-                                key={currency.symbol}
-                                value={currency.symbol}>
-                                {currency.name}
-                            </option>
-                        ))};
-                    </select>
-                </label>
-            </fieldset>
-            <button className="form__button">Calculate</button>
-            <Result result={result} />
-        </form>
-    );
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <img src="/images/header_logo.png" alt="Logo" className="form__logo" />
+      <legend className="form__legend form--separator">{title}</legend>
+      <label className="form__label">
+        <span className="form__label--description">Amount in PLN:</span>
+        <input
+          className="form__input"
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
+          type="number"
+          placeholder="Enter the amount to exchange"
+          step="0.01"
+          max="10000000000"
+          min="0.01"
+          required
+        />
+      </label>
+      <label className="form__label">
+        <span className="form__label--description">Choose currency:</span>
+        <select
+          className="form__select"
+          value={selectedCurrency}
+          onChange={({ target }) => setSelectedCurrency(target.value)}
+          name="currency"
+          required
+        >
+          {currencies.map((currency) => (
+            <option key={currency.symbol} value={currency.symbol}>
+              {currency.name}
+            </option>
+          ))}
+          ;
+        </select>
+      </label>
+
+      <button className="form__button">
+        <img
+          className="form__button--icon"
+          src="/images/exchange_icon.png"
+          alt="exchange_icon"
+        />
+        Calculate
+      </button>
+      <Result result={result} />
+    </form>
+  );
 };
 
 export default Form;
