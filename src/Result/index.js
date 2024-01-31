4841713e5dca18012c3981354093b88144ca8464
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-31 22:19:00 +0100

    refactored Result component with new logic and styling

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,12 +1,20 @@
 import "./style.css";
 
-export const Result = ({ result }) => (
-    <p className="result">
-        {result !== undefined && (
-            <>
-                {result.sourceAmount.toFixed(2)} PLN ={" "}
-                <strong>{result.targetAmount.toFixed(2)} {result.currency}</strong>
-            </>
-        )}
-    </p>
-);
+export const Result = ({ result }) => {
+  return (
+    <div>
+      {result !== undefined && (
+        <div className="result">
+          <p className="result__pln">{result.sourceAmount.toFixed(2)} PLN</p>
+          <p className="result__info">After calculation:</p>
+          <p>
+            <strong className="result__exchanged">
+              {result.targetAmount.toFixed(2)} {result.currency}
+            </strong>
+          </p>
+          <img className="result__flag" src={result.flag} alt="Flag" />
+        </div>
+      )}
+    </div>
+  );
+};
