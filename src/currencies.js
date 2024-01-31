231da483c2291a275eaefbc8ad4f0be6803fed52
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-01-31 22:20:18 +0100

    added flag atribute with file path to every object in currencies array

--- src/currencies.js
+++ src/currencies.js
@@ -1,17 +1,20 @@
 export const currencies = [
-    {
-        name: "EUR - Euro",
-        ratio: 4.36,
-        symbol: "EUR",
-    },
-    {
-        name: "USD - Dolar amerykański",
-        ratio: 4.03,
-        symbol: "USD",
-    },
-    {
-        name: "GBP - Funt brytyjski",
-        ratio: 5.10,
-        symbol: "GBP",
-    },
+  {
+    name: "EUR - Euro",
+    ratio: 4.36,
+    symbol: "EUR",
+    flag: "/images/flags/EUR.png",
+  },
+  {
+    name: "USD - U.S. Dollar",
+    ratio: 4.03,
+    symbol: "USD",
+    flag: "/images/flags/USD.png",
+  },
+  {
+    name: "GBP - British pound sterling",
+    ratio: 5.1,
+    symbol: "GBP",
+    flag: "/images/flags/GBP.png",
+  },
 ];
