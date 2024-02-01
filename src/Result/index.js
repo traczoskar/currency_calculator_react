9db63ad38d477f065d4ebb47cca028e4a2ffacfa
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-01 13:22:09 +0100

    shorted the syntax of Result function

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,20 +1,18 @@
 import "./style.css";
 
-export const Result = ({ result }) => {
-  return (
-    <div>
-      {result !== undefined && (
-        <div className="result">
-          <p className="result__pln">{result.sourceAmount.toFixed(2)} PLN</p>
-          <p className="result__info">After calculation:</p>
-          <p>
-            <strong className="result__exchanged">
-              {result.targetAmount.toFixed(2)} {result.currency}
-            </strong>
-          </p>
-          <img className="result__flag" src={result.flag} alt="Flag" />
-        </div>
-      )}
-    </div>
-  );
-};
+export const Result = ({ result }) => (
+  <div>
+    {result && (
+      <div className="result">
+        <p className="result__pln">{result.sourceAmount.toFixed(2)} PLN</p>
+        <p className="result__info">After calculation:</p>
+        <p>
+          <strong className="result__exchanged">
+            {result.targetAmount.toFixed(2)} {result.currency}
+          </strong>
+        </p>
+        <img className="result__flag" src={result.flag} alt="Flag" />
+      </div>
+    )}
+  </div>
+);
