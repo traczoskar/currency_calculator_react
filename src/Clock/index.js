2a41ea63f0568ee780acb3a281a01eb37d9723c1
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-03 10:08:44 +0100

    formatting the date moved to new cons "formattedDate"

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -3,7 +3,15 @@ import "./style.css";
 
 export const Clock = () => {
   const [date, setDate] = useState(new Date());
-
+  const formattedDate = date.toLocaleString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
   useEffect(() => {
     const intervalId = setInterval(() => {
       setDate(new Date());
@@ -17,17 +25,7 @@ export const Clock = () => {
   return (
     <p className="clock">
       Today is&nbsp;
-      <strong className="clock__prompt">
-        {date.toLocaleString("en-US", {
-          weekday: "long",
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        })}
-      </strong>
+      <strong className="clock__prompt">{formattedDate}</strong>
     </p>
   );
 };
