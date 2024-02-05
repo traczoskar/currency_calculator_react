bb8e2e407dd33c36441cbd55f64c2cfcfd8aec4b
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-05 14:32:21 +0100

    added few colors to the theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,10 @@ export const theme = {
     alabaster: "#f8f8f8",
     silver: "#cccccc",
     darksilver: "#bbbbbb",
+    mercury: "#e8e8e8",
+    mineshaft: "#333333",
+    emperor: "#555555",
+    boulder: "#777777",
     black: "#000000",
   },
   breakpoint: {
