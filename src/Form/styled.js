f7fc25dcc9119509bbe95046b4371c5a19b046f0
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-05 14:33:32 +0100

    changed colors for theme callbacks, also deleted unnecesary media queries

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -11,10 +11,6 @@ export const Wrapper = styled.form`
   border: 1px solid ${({ theme }) => theme.color.silver};
   border-radius: 5px;
   box-shadow: 2px 3px 20px rgba(0, 0, 0, 0.1);
-
-  @media (max-width: 767px) {
-    margin: 10px 5px;
-  }
 `;
 
 export const HeaderLogo = styled.img`
@@ -64,14 +60,14 @@ export const Label = styled.label`
   width: 100%;
   margin-top: 15px;
 
-  @media (max-width: 767px) {
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     grid-template-columns: 1fr;
     text-align: center;
   }
 `;
 
 export const InputName = styled.span`
-  color: #555;
+  color: ${({ theme }) => theme.color.emperor};
   font-weight: 600;
   font-size: 15px;
   text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.135);
@@ -81,11 +77,11 @@ export const InputWindow = styled.input`
   font-size: 15px;
   width: 100%;
   padding: 15px;
-  border: 1px solid #bbb;
-  background-color: #e8e8e8;
+  border: 1px solid ${({ theme }) => theme.color.darksilver};
+  background-color: ${({ theme }) => theme.color.mercury};
   box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.1);
   border-radius: 5px;
-  color: #333;
+  color: ${({ theme }) => theme.color.mineshaft};
   cursor: pointer;
 `;
 
@@ -100,13 +96,13 @@ export const Button = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 1px solid #ccc;
+  border: 1px solid ${({ theme }) => theme.color.silver};
   border-radius: 15px;
-  background-color: #e2e2e2;
+  background-color: ${({ theme }) => theme.color.mercury};
   box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.1);
   text-shadow: 3px 3px 10px rgba(0, 0, 0, 0.135);
   padding: 10px 10px;
-  color: #222;
+  color: ${({ theme }) => theme.color.mineshaft};
   font-size: 18px;
   font-weight: 700;
   width: 100%;
@@ -117,7 +113,6 @@ export const Button = styled.button`
 
   &:hover {
     background-color: hsl(89, 42%, 62%);
-    color: #333;
     transform: scale(1.03);
     ${CalculateIcon} {
       transform: rotate(360deg) scale(1.2);
@@ -126,6 +121,5 @@ export const Button = styled.button`
 
   &:active {
     background-color: hsl(60, 1%, 30%);
-    color: #333;
   }
 `;
