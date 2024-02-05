961f3d281afe05eaf2a2cd55db5886cb3a95f197
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-05 15:25:34 +0100

    added imports for pictures of flags

--- src/currencies.js
+++ src/currencies.js
@@ -1,20 +1,24 @@
+import EUR from "./images/flags/EUR.png";
+import GBP from "./images/flags/GBP.png";
+import USD from "./images/flags/USD.png";
+
 export const currencies = [
   {
     name: "EUR - Euro",
     ratio: 4.36,
     symbol: "EUR",
-    flag: "./images/flags/EUR.png",
+    flag: EUR,
   },
   {
     name: "USD - U.S. Dollar",
     ratio: 4.03,
     symbol: "USD",
-    flag: "./images/flags/USD.png",
+    flag: USD,
   },
   {
     name: "GBP - British pound sterling",
     ratio: 5.1,
     symbol: "GBP",
-    flag: "./images/flags/GBP.png",
+    flag: GBP,
   },
 ];
