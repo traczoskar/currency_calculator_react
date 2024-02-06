823cca756c8fa5b7a77576dfdf566ca7cd27d8e5
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-06 14:52:31 +0100

    moved Data formatting out of the useCurrentDate Hook

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,7 +2,17 @@ import { useCurrentDate } from "./useCurrentDate";
 import { ClockContainer, ClockPrompt } from "./styled";
 
 export const Clock = () => {
-  const formattedDate = useCurrentDate();
+  const date = useCurrentDate();
+
+  const formattedDate = date.toLocaleString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
 
   return (
     <ClockContainer>
--- src/Clock/useCurrentDate.js
+++ src/Clock/useCurrentDate.js
@@ -2,15 +2,7 @@ import { useState, useEffect } from "react";
 
 export const useCurrentDate = () => {
   const [date, setDate] = useState(new Date());
-  const formattedDate = date.toLocaleString("en-US", {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
+
   useEffect(() => {
     const intervalId = setInterval(() => {
       setDate(new Date());
@@ -21,5 +13,5 @@ export const useCurrentDate = () => {
     };
   }, []);
 
-  return formattedDate;
+  return date;
 };
