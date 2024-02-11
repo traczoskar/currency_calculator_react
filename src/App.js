26cc853f42cda2c75038ef8560c2320b5876fd65
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-11 22:44:29 +0100

    moved logic and calculateResult function to Form component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import { useState } from "react";
 import Form from "./Form";
-import { currencies } from "./currencies";
 
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (selectedCurrency, amount) => {
-    const currencyElement = currencies.find(
-      (currency) => currency.symbol === selectedCurrency
-    );
-    const currencyRate = currencyElement.ratio;
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / currencyRate,
-      currency: selectedCurrency,
-      flag: currencyElement.flag,
-    });
-  };
-
   return (
     <main>
-      <Form
-        title="CURRENCY CALCULATOR"
-        calculateResult={calculateResult}
-        result={result}
-      />
+      <Form title="CURRENCY CALCULATOR" />
     </main>
   );
 }
