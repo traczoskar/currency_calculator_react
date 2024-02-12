13f3517fb8d5d9147b7fd323dfac0f93f067a8f9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 09:46:32 +0100

    changed API URL to outside API key

--- src/Form/getApiData.js
+++ src/Form/getApiData.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const getApiData = async () => {
   try {
-    const response = await axios.get("./exampleJSON.json");
+    const response = await axios.get(
+      "https://api.currencyapi.com/v3/latest?apikey=cur_live_dJIiMBbmAZ9HJeKBX41En6P3lzSmOo04hm1Bg2m9"
+    );
     return response.data;
   } catch (error) {
     console.error("Error while fetching data:", error.message);
