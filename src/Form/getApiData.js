307698b273f594fa17e5f0626aa7793eb8c1d45c
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 16:07:16 +0100

    moved the API endpoint URL to separate const outside of the function

--- src/Form/getApiData.js
+++ src/Form/getApiData.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+const apiURL =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_dJIiMBbmAZ9HJeKBX41En6P3lzSmOo04hm1Bg2m9";
+
 export const getApiData = async () => {
   try {
-    const response = await axios.get(
-      "https://api.currencyapi.com/v3/latest?apikey=cur_live_dJIiMBbmAZ9HJeKBX41En6P3lzSmOo04hm1Bg2m9"
-    );
+    const response = await axios.get(apiURL);
     return response.data;
   } catch (error) {
     console.error("Error while fetching data:", error.message);
