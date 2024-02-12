5fe176de31ab7d0686bd3f3d9717938fc92eda1d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-02-12 08:31:58 +0100

    deleted unnecesarry console logs and corrected error catching

--- src/Form/getApiData.js
+++ src/Form/getApiData.js
@@ -3,11 +3,8 @@ import axios from "axios";
 export const getApiData = async () => {
   try {
     const response = await axios.get("./exampleJSON.json");
-    console.log("Calling getApiData", response);
-    console.log("exampleJSON content:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error while fetching data:", error);
-    throw error;
+    console.error("Error while fetching data:", error.message);
   }
 };
