37da3188865abd83700f6a56c410f27a51518231
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-13 15:16:49 +0200

    removed graphic declarations to another file

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,18 +1,3 @@
-declare module "*.png" {
-  const value: any;
-  export = value;
-}
-
-declare module "*.jpeg" {
-  const value: any;
-  export = value;
-}
-
-declare module "*.jpg" {
-  const value: any;
-  export = value;
-}
-
 export interface CurrencyData {
   status: string;
   rates?: { [key: string]: { code: string; value: number } };
