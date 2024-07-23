9aa2f7b7cfbfecfadc155d6b8856a2b8ef3fa69d
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-23 16:42:18 +0200

    tests: added test for changing the status after fetching API data

--- src/hooks/__tests__/useCurrencyData.test.ts
+++ src/hooks/__tests__/useCurrencyData.test.ts
@@ -1,5 +1,6 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { useCurrencyData } from "hooks/useCurrencyData";
+import { getApiData } from "utils/getApiData";
 
 jest.mock("utils/getApiData");
 
@@ -15,7 +16,29 @@ describe("useCurrencyData hook tests", () => {
 
   test("should return status loading firstly", () => {
     const { result } = renderHook(() => useCurrencyData());
-    expect(result.current.status).toBe("loading");
-    expect(result.current.rates).toBeUndefined();
+    expect(result.current).toEqual({ status: "loading", rates: undefined });
+  });
+
+  test("should return status downloaded after fetching the data", async () => {
+    const mockData = {
+      meta: { last_updated_at: "2023-07-23T23:59:59Z" },
+      data: {
+        USD: { code: "USD", value: 1 },
+        EUR: { code: "EUR", value: 0.89 },
+      },
+    };
+    (getApiData as jest.Mock).mockResolvedValue(mockData);
+    const { result } = renderHook(useCurrencyData);
+
+    act(() => {
+      jest.advanceTimersByTime(3500);
+    });
+    await waitFor(() => {
+      expect(result.current.status).toBe("downloaded");
+    });
+    expect(result.current).toEqual({
+      status: "downloaded",
+      rates: mockData.data,
+    });
   });
 });
