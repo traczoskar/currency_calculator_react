af60705e5ab5e0cc2aab7fb203cf2a315ad0e2d9
traczoskar <155188539+traczoskar@users.noreply.github.com>
2024-07-23 16:20:04 +0200

    chore: Add ApiResponse interface for API response data

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,6 +3,11 @@ export interface CurrencyData {
   rates?: { [key: string]: { code: string; value: number } };
 }
 
+export interface ApiResponse {
+  data: { [key: string]: { code: string; value: number } };
+  meta: { last_updated_at: string };
+}
+
 export interface ResultData {
   sourceAmount: number;
   targetAmount: number;
